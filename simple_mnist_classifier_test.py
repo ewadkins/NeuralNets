12263f7615097b041683e187a04ef7d4fd36737b
ewadkins <ewadkins83@gmail.com>
2017-01-23 22:18:08 -0500

    Modified simple MNIST classifier

--- simple_mnist_classifier_test.py
+++ simple_mnist_classifier_test.py
@@ -6,30 +6,7 @@ from mnist import MNIST
 import math
 import time
 from display_utils import DynamicConsoleTable
-
-print 'Loading images..'
-mndata = MNIST('./mnist')
-training_images, training_labels = mndata.load_training()
-validation_images, validation_labels = mndata.load_testing()
-print 'Training images: {}'.format(len(training_images))
-print 'Validation images: {}'.format(len(validation_images))
-
-print 'Reshaping images..'
-for i in range(len(training_images)):
-    training_images[i] = np.expand_dims(np.reshape(training_images[i], (28, 28)), axis=2)
-for i in range(len(validation_images)):
-    validation_images[i] = np.expand_dims(np.reshape(validation_images[i], (28, 28)), axis=2)
-print 'Done'
-
-#fig = plt.figure()
-#for i in range(len(training_images)):
-#    print "{} / {}".format(i + 1, len(training_images))
-#    fig.clear()
-#    plt.imshow(training_images[i], cmap='gray')
-#    plt.pause(0.001)
-
-#plt.imshow(training_images[0], cmap='gray')
-#plt.show()
+from sound_utils import Sounds
 
 ###############################################################################
 
@@ -74,30 +51,152 @@ def model(learning_rate=0.01):
 max_epochs = 1000
 batch_size = 100
 validation_set_size = 1000
-learning_rate = 0.05
+learning_rate = 0.01
 loss_threshold = 0 #1e-12
 decay_rate = 0.30 # Exponential decay used to calculate sustained loss
 use_GPU = True # Use CUDA acceleration
 
-# Display settings
-show_progress = False
-display_step = 50 # in batches
-delay = 0.001
+# Weights display settings
+show_weights = False
+weights_display_step = 50 # in batches
 interpolation = None # None to use default (eg. "nearest", "bilinear")
 cmap = None # None to use default (eg. "gray", "inferno")
+weights_delay = 0.001
+weights_title = 'Weights'
+
+# Loss display settings
+show_loss = False
+loss_display_step = 50
+loss_saved_iterations = 500
+loss_starting_iteration = 20
+loss_delay = 0.001
+loss_title = 'Loss'
+
+# Accuracy display settings
+show_accuracy = False
+accuracy_display_step = 50
+accuracy_saved_iterations = float('inf')
+accuracy_starting_iteration = 0
+accuracy_delay = 0.001
+accuracy_title = 'Accuracy'
+
+# Console output settings
 progress_bar_size = 20
 
+# Sound settings
+use_sounds = True
+
+##############################################################################
+# Data loader and setup
+print 'Loading images..'
+mndata = MNIST('./mnist')
+training_images, training_labels = mndata.load_training()
+validation_images, validation_labels = mndata.load_testing()
+print 'Training images: {}'.format(len(training_images))
+print 'Validation images: {}'.format(len(validation_images))
+
+print 'Reshaping images..'
+for i in range(len(training_images)):
+    training_images[i] = np.expand_dims(np.reshape(training_images[i], (28, 28)), axis=2)
+for i in range(len(validation_images)):
+    validation_images[i] = np.expand_dims(np.reshape(validation_images[i], (28, 28)), axis=2)
+    
+print 'Creating batches..'
+assert validation_set_size <= len(validation_images), 'validation_set_size must be smaller than len(validation_images)'
+assert float(len(training_images)) / batch_size % 1 == 0, 'batch_size must evenly divide len(training_images)'
+assert float(validation_set_size) / batch_size % 1 == 0, 'batch_size must evenly divide validation_set_size'
+num_training_batches = len(training_images) / batch_size
+num_validation_batches = validation_set_size / batch_size
+
+training_image_batches = []
+training_label_batches = []
+validation_image_batches = []
+validation_label_batches = []
+for i in range(num_training_batches):
+    training_image_batches.append(training_images[i*batch_size:(i+1)*batch_size])
+    training_label_batches.append(training_labels[i*batch_size:(i+1)*batch_size])
+for i in range(num_validation_batches):
+    validation_image_batches.append(validation_images[i*batch_size:(i+1)*batch_size])
+    validation_label_batches.append(validation_labels[i*batch_size:(i+1)*batch_size])
+    
+print 'Done'
 ###############################################################################
-# Display setup
-fig = None
-def display(weights_val):
-    fig.clear()
+
+###############################################################################
+# Pyplot setup
+plt.ion() # Enable interactive mode
+###############################################################################
+
+###############################################################################
+# Progress display setup
+weights_fig = None
+if show_weights:
+    weights_fig = plt.figure(weights_title)
+    weights_fig.show()
+def display_weights(weights_val):
+    weights_fig = plt.figure(weights_title)
+    weights_fig.clear()
     plot_height = int(weights_val['conv1'].shape[3] ** 0.5)
     plot_width = math.ceil(float(weights_val['conv1'].shape[3]) / plot_height)
     for j in range(weights_val['conv1'].shape[3]):
-        ax = fig.add_subplot(plot_height, plot_width, j + 1)
+        ax = weights_fig.add_subplot(plot_height, plot_width, j + 1)
         ax.imshow(weights_val['conv1'][:,:,0,j], interpolation=interpolation, cmap=cmap)
-    plt.pause(delay)
+    plt.pause(weights_delay)
+###############################################################################
+
+###############################################################################
+# Loss display setup
+loss_fig = None
+if show_loss:
+    loss_fig = plt.figure(loss_title)
+    loss_fig.show()
+def display_loss(loss_values, iteration):
+    loss_fig = plt.figure(loss_title)
+    loss_fig.clear()
+    plt.plot(list(range(iteration - len(loss_values) + 1, iteration + 1)), loss_values)
+    plt.pause(loss_delay)
+###############################################################################
+
+###############################################################################
+# Accuracy display setup
+accuracy_fig = None
+if show_loss:
+    accuracy_fig = plt.figure(accuracy_title)
+    accuracy_fig.show()
+def display_accuracy(validation_accuracy_values, max_accuracy_values, iteration):
+    accuracy_fig = plt.figure(accuracy_title)
+    accuracy_fig.clear()
+    plt.plot(list(range(iteration - len(validation_accuracy_values) + 1, iteration + 1)), validation_accuracy_values)
+    #plt.plot(list(range(iteration - len(max_accuracy_values) + 1, iteration + 1)), max_accuracy_values)
+    #plt.plot([iteration - len(max_accuracy_values) + 1, iteration], [max_accuracy_values[-1]] * 2)
+    plt.pause(accuracy_delay)
+###############################################################################
+
+###############################################################################
+# Sound setup
+sounds = Sounds()
+if use_sounds:
+    sounds.open()
+###############################################################################
+
+###############################################################################
+# Output control
+def update_output(iteration, weights_val, loss_values, validation_accuracy_values, max_accuracy_values, override=False):
+    
+    # Show/update weights display
+    if iteration % weights_display_step == 0 and show_weights or override:
+        display_weights(weights_val)
+        if use_sounds:
+            sounds.alert()
+
+    # Show/update loss display
+    if iteration % loss_display_step == 0 and show_loss or override:
+        display_loss(loss_values, iteration)
+
+    # Show/update accuracy display
+    if iteration % accuracy_display_step == 0 and show_accuracy or override:
+        display_accuracy(validation_accuracy_values, max_accuracy_values, iteration)
+        
 ###############################################################################
 
 # Build model and get variable handles
@@ -110,42 +209,29 @@ initialize = tf.global_variables_initializer()
 config = tf.ConfigProto(device_count = {'GPU': 1 if use_GPU == True else 0})
 
 # Run model
+done = False
+epoch = 0
+iteration = 0
+sustained_loss = 0.0
+loss_values = []
+validation_accuracy_values = []
+max_accuracy_values = []
+
+max_accuracy = 0.0
+max_accuracy_weights = None
+max_accuracy_biases = None
+    
 with tf.Session(config=config) as session:
     session.run(initialize)
-        
-    done = False
-    epoch = 0
-    iteration = 0
-    sustained_loss = 0.0
-    loss_values = []
     
-    assert validation_set_size <= len(validation_images), 'validation_set_size must be smaller than len(validation_images)'
-    assert float(len(training_images)) / batch_size % 1 == 0, 'batch_size must evenly divide len(training_images)'
-    assert float(validation_set_size) / batch_size % 1 == 0, 'batch_size must evenly divide validation_set_size'
-    num_training_batches = len(training_images) / batch_size
-    num_validation_batches = validation_set_size / batch_size
-    
-    training_image_batches = []
-    training_label_batches = []
-    validation_image_batches = []
-    validation_label_batches = []
-    for i in range(num_training_batches):
-        training_image_batches.append(training_images[i*batch_size:(i+1)*batch_size])
-        training_label_batches.append(training_labels[i*batch_size:(i+1)*batch_size])
-    for i in range(num_validation_batches):
-        validation_image_batches.append(validation_images[i*batch_size:(i+1)*batch_size])
-        validation_label_batches.append(validation_labels[i*batch_size:(i+1)*batch_size])
-    
-    max_validation_accuracy = 0.0
-    max_accuracy_weights = None
-    max_accuracy_biases = None
-    
-    if show_progress:
-        fig = plt.figure()
-        weights_val = session.run(weights)
-        display(weights_val)
-        
+    print '=========='
+    print 'GPU ' + ('enabled' if use_GPU else 'disabled')
     print
+    
+    # Show weight initialization
+    if show_weights:
+        weights_val = session.run(weights)
+        display_weights(weights_val)
     
     layout = [
         dict(name='Ep.', width=3, align='center'),
@@ -160,6 +246,9 @@ with tf.Session(config=config) as session:
     
     while not done:
         epoch += 1
+        
+        if use_sounds:
+            sounds.alert()
 
         # Trains on the data, in batches
         for i in range(num_training_batches):
@@ -169,7 +258,11 @@ with tf.Session(config=config) as session:
             labels_batch = training_label_batches[i]
             _, loss_val = session.run([train_op, loss], feed_dict={x: images_batch, y: labels_batch})
             sustained_loss = decay_rate * sustained_loss + (1.0 - decay_rate) * loss_val
-            loss_values.append(loss_val)
+            
+            if len(loss_values) == loss_saved_iterations:
+                loss_values.pop(0)
+            if iteration >= loss_starting_iteration:
+                loss_values.append(loss_val)
             
             images_batch = validation_image_batches[iteration % num_validation_batches]
             labels_batch = validation_label_batches[iteration % num_validation_batches]
@@ -182,11 +275,23 @@ with tf.Session(config=config) as session:
                 validation_accuracy += accuracy_val
             validation_accuracy /= num_validation_batches
             
-            if validation_accuracy > max_validation_accuracy:
+            if len(validation_accuracy_values) == accuracy_saved_iterations:
+                validation_accuracy_values.pop(0)
+            if iteration >= accuracy_starting_iteration:
+                validation_accuracy_values.append(validation_accuracy)
+            
+            if validation_accuracy > max_accuracy:
                 weights_val, biases_val = session.run([weights, biases])
-                max_validation_accuracy = validation_accuracy
+                max_accuracy = validation_accuracy
                 max_accuracy_weights = weights_val
                 max_accuracy_biases = biases_val
+                if use_sounds:
+                    sounds.success()
+                    
+            if len(max_accuracy_values) == accuracy_saved_iterations:
+                max_accuracy_values.pop(0)
+            if iteration >= accuracy_starting_iteration:
+                max_accuracy_values.append(max_accuracy)
             
             progress = int(math.ceil(progress_bar_size * float((iteration - 1) % num_training_batches) / (num_training_batches - 1)))
             progress_string = '[' + '#' * progress + ' ' * (progress_bar_size - progress) + ']'
@@ -196,7 +301,7 @@ with tf.Session(config=config) as session:
                          (iteration - 1) % num_training_batches + 1,
                          sustained_loss,
                          validation_accuracy * 100,
-                         max_validation_accuracy * 100,
+                         max_accuracy * 100,
                          progress_string)
             
             # Termination condition
@@ -204,22 +309,13 @@ with tf.Session(config=config) as session:
                 done = True
                 break
 
-            # Show/update display
-            if iteration % display_step == 0 and show_progress:
-                display(weights_val)
+            update_output(iteration, weights_val, loss_values, validation_accuracy_values, max_accuracy_values)
         
         table.finalize()
             
         # Termination condition
         if epoch >= max_epochs or sustained_loss < loss_threshold:
             done = True
-            if show_progress:
-                display(weights_val)
-            
-# Display results
-plt.show()
-
-#plt.figure('Loss')
-#plt.plot(loss_values)
-#plt.show()
+            update_output(iteration, weights_val, loss_values, validation_accuracy_values, max_accuracy_values, override=True)
+            plt.pause(0)
 
